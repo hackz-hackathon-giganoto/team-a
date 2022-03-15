842619b2cd01b43f37579ca6067862a6cf6d1bb3
yukinissie <bigtree2490@outlook.jp>
2022-03-15 15:07:44 +0900

    Auth周りを修正

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -12,19 +12,19 @@ const NavBar = ({ user }) => {
       {!user && (
         <span>
           <a
-            href="/.auth/login/google?post_login_redirect_uri=${redirect}"
+            href={`/.auth/login/google?post_login_redirect_uri=${redirect}`}
             className="btn btn-danger btn-block"
           >
             <i className="fa fa-google" /> Sign in with <b>Google</b>
           </a>
           <a
-            href="/.auth/login/twitter?post_login_redirect_uri=${redirect}"
+            href={`/.auth/login/twitter?post_login_redirect_uri=${redirect}`}
             className="btn btn-info btn-block"
           >
             <i className="fa fa-twitter" /> Sign in with <b>Twitter</b>
           </a>
           <a
-            href="/.auth/login/github?post_login_redirect_uri=${redirect}"
+            href={`/.auth/login/github?post_login_redirect_uri=${redirect}`}
             className="btn btn-dark btn-block"
           >
             <i className="fa fa-github" /> Sign in with <b>Github</b>
