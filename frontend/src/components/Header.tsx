ae25d0a714ed6c8d6c02d9986ec8bdbb0bc7cf3c
yukinissie <bigtree2490@outlook.jp>
2022-03-15 15:13:53 +0900

    表示をちょっと修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = (props: HeaderProps) => {
         <div className='d-flex justify-content-between header-container'>
             <p className="header-item">奇声を発してハッピーハッピー</p>
             <div>
-                <p className="header-item">{props.state} /`接続ユーザー数: {props.user_count}人</p>
+                <p className="header-item">{props.state} / 接続ユーザー数: {props.user_count}人</p>
                 {/*<p className="header-item">{`Pod数: ${props.pod_count}`}</p>*/}
 
             </div>
