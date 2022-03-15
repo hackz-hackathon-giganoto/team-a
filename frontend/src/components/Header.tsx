7e02acee5d0a4f44a2d8b35bc80bde0857864c2e
yukinissie <bigtree2490@outlook.jp>
2022-03-15 15:50:10 +0900

    コードのフォーマット（GitHubActionsのキックのため）

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
 interface HeaderProps {
-    state: string;
-    user_count:number;
-    pod_count:number;
+  state: string;
+  user_count: number;
+  pod_count: number;
 }
 
 const Header = (props: HeaderProps) => {
-    return (
-        <div className='d-flex justify-content-between header-container'>
-            <p className="header-item">奇声を発してハッピーハッピー</p>
-            <div>
-                <p className="header-item">{props.state} / 接続ユーザー数: {props.user_count}人</p>
-                {/*<p className="header-item">{`Pod数: ${props.pod_count}`}</p>*/}
-
-            </div>
-        </div>
-    );
-}
-
+  return (
+    <div className="d-flex justify-content-between header-container">
+      <p className="header-item">奇声を発してハッピーハッピー</p>
+      <div>
+        <p className="header-item">
+          {props.state} / 接続ユーザー数: {props.user_count}人
+        </p>
+        {/*<p className="header-item">{`Pod数: ${props.pod_count}`}</p>*/}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
